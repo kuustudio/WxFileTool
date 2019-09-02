dd2d34695a45675bdd423eda4db76c69c201fb0a
SurrealSky <40445799+SurrealSky@users.noreply.github.com>
2019-09-02 21:38:35 +0800

    更新

--- WxFileTool/stdafx.h
+++ WxFileTool/stdafx.h
@@ -36,7 +36,7 @@
 #include <afxcmn.h>                     // MFC 对 Windows 公共控件的支持
 #endif // _AFX_NO_AFXCMN_SUPPORT
 
-#include "../CommonLib/include/PluginLibrary.h"
+#include "../CommonLib/include/CipherManager/PluginLibrary.h"
 
 #include"../CommonLib/include/HexControlInclude.h"
 #ifdef _DEBUG
